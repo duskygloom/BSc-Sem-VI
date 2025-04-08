02f8c00519279b00dadc78b8565a899207c65454
duskygloom <sarkarharsh708@gmail.com>
2025-04-08 16:53:15 +0530

    macros makes life easier

--- graphics_programming/midpoint_circle.c
+++ graphics_programming/midpoint_circle.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
+#define BIG_CIRCLE
+#define PRINT_TABLE
+
 void drawCircle(int r, int cx, int cy) {
+    #ifndef BIG_CIRCLE
+    r *= 10;
+    cx *= 10;
+    cy *= 10;
+    #endif
+
     int x = 0, y = r;
 
     int p = 1 - r;
@@ -20,19 +29,19 @@ void drawCircle(int r, int cx, int cy) {
         glVertex2d(cx - y, cy - x);
         glEnd();
         
-#ifdef PRINT_TABLE
+        #ifdef PRINT_TABLE
         printf("%d\t", k++);
         printf("%d\t", p);
-#endif
+        #endif
         x += 1;
         if (p >= 0) {
             y -= 1;
             p -= 2*y;
         }
         p += 2*x + 1;
-#ifdef PRINT_TABLE
+        #ifdef PRINT_TABLE
         printf("(%d\t%d)\n", x, y);
-#endif
+        #endif
     }
 }
 
@@ -40,29 +49,30 @@ void displayFunc(void) {
     glClearColor(1, 1, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(0, 0, 0);
-    glPointSize(2);
-    drawCircle(200, 400, 300);
-
-    glFlush();
-
-    return;
-
+    glColor3f(0, 1, 0);
     glBegin(GL_LINES);
+    #ifdef BIG_CIRCLE
     glVertex2d(0, -30);
     glVertex2d(0, 30);
+    #else
+    glVertex2d(0, -300);
+    glVertex2d(0, 300);
+    #endif
     glEnd();
 
     glBegin(GL_LINES);
+    #ifdef BIG_CIRCLE
     glVertex2d(-40, 0);
     glVertex2d(40, 0);
+    #else
+    glVertex2d(-400, 0);
+    glVertex2d(400, 0);
+    #endif
     glEnd();
 
-    glPointSize(10);
-
-#ifdef PRINT_TABLE
+    #ifdef PRINT_TABLE
     printf("Begins:\n");
-#endif
+    #endif
     glColor3f(0, 0, 0);
     drawCircle(16, 3, 4);
 
@@ -82,8 +92,13 @@ int main(int argc, char **argv) {
     glutCreateWindow("Midpoint circle");
 
     glMatrixMode(GL_PROJECTION);
-    // gluOrtho2D(-40, 40, -30, 30);
-    gluOrtho2D(0, 800, 0, 600);
+    #ifdef BIG_CIRCLE
+    gluOrtho2D(-40, 40, -30, 30);
+    glPointSize(10);
+    #else
+    gluOrtho2D(-400, 400, -300, 300);
+    glPointSize(1);
+    #endif
 
     glutDisplayFunc(displayFunc);
 
