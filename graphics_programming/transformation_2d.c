842014ce1f74450cc286a84b85d6d181aafae0f1
duskygloom <sarkarharsh708@gmail.com>
2025-04-16 10:04:07 +0530

    Let's just define pi and get it done with.

--- graphics_programming/transformation_2d.c
+++ graphics_programming/transformation_2d.c
@@ -1,12 +1,10 @@
-#define __USE_MISC // to include math.h constants
-#define _USE_MATH_DEFINES // include constants in windows
-
 #include <math.h>
 #include <stdio.h>
 #include <GL/glut.h>
 
 typedef float Matrix3x3[3][3];
 
+#define M_PI 3.14159265358979323846
 #define DEG_TO_RAD(x) (x * M_PI / 180.0)
 
 #define BIG_POINTS
