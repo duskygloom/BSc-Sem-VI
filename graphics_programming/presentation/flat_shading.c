2224a18cad195da06b381b05b8719ed70604ae60
duskygloom <sarkarharsh708@gmail.com>
2025-05-13 15:46:19 +0530

    Why was the yellow light commented?

--- graphics_programming/presentation/flat_shading.c
+++ graphics_programming/presentation/flat_shading.c
@@ -13,8 +13,8 @@ int DRAW_WORLD_AXES = 1;
 /* About light sources. */
 vector_t lights[] = {
     /* Position                 Color                   Brightness (only .x is used)*/
-    //    {-500, 400, -300},       {0xff, 0xe9, 0x48},     {0.4, 0, 0},
-       {600, 600, 600},         {0xff, 0xff, 0xff},     {0.2, 0, 0},
+       {-500, 400, -300},       {0xff, 0xe9, 0x48},     {0.4, 0, 0},
+       {600, 600, 600},         {0xff, 0xff, 0xff},     {0.1, 0, 0},
 };
 
 const int nLights = sizeof(lights) / sizeof(vector_t) / 3;
